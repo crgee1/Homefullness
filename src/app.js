55beccaf64e5314a27d2ef759d5e6c445302042c
crgee1 <christopherrgee@gmail.com>
2019-07-18 09:56:16 -0700

    Work with canvas

---
+++ src/app.js
@@ -0,0 +1,15 @@
+
+// document.addEventListener("DOMContentLoaded", () => {
+//   const canvasEl = document.getElementsByTagName("canvas")[0];
+//   canvasEl.width = 500;
+//   canvasEl.height = 500;
+
+//   const ctx = canvasEl.getContext("2d");
+//   imageObj = new Image();
+//   imageObj.onLoad = function() {
+//     ctx.drawImage(imageObj, 69, 50);
+//   }
+  
+//   imageObj.src = '../assets/images/person-icon.png';
+//   imageObj.onLoad();
+// });
---
+++ src/sprite.js
@@ -0,0 +1,2 @@
+const sprite = new Image();
+sprite.src = "../assets/images/person-icon.png";
