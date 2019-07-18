60fe8d06d6e124615c32847d30160ad1db8657cb
crgee1 <christopherrgee@gmail.com>
2019-07-17 23:13:45 -0700

    Delete module in webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -7,20 +7,6 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
   },
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /(node_modules)/,
-        use: {
-          loader: 'babel-loader',
-          query: {
-            presets: ['@babel/env', '@babel/react']
-          }
-        },
-      }
-    ]
-  },
   devtool: 'inline-source-map',
   resolve: {
     extensions: [".js", ".jsx", "*"]
