aeee7ac7c26be045baee494efcf7d1386346b7f3
crgee1 <christopherrgee@gmail.com>
2019-08-19 14:22:21 -0700

    Implement more principles of OOP

--- src/person.js
+++ src/person.js
@@ -1,17 +1,23 @@
+const CONSTANTS = {
+  startX: 450,
+  startY: 330,
+  speed: 2,
+}
+
 export default class Person {
   constructor(ctx, ethnicity, n) {
     this.ctx = ctx;
     this.person = new Image();
     this.person.src = `./assets/images/person-${ethnicity}.png`;
-    this.startY = 330;
-    this.startX = 450;
-    this.endX = 0 + 20 * (n % 12);
-    this.endY = 0 + 40 * Math.floor(n / 12);
+    this.startY = CONSTANTS.startY;
+    this.startX = CONSTANTS.startX;
+    this.endX = 20 * (n % 12);
+    this.endY = 40 * Math.floor(n / 12);
     this.animate = this.animate.bind(this);
   }
 
   animate() {
-    const speed = 2;
+    const speed = CONSTANTS.speed;
     this.ctx.drawImage(this.person, this.startX, this.startY, 50, 50);
     let slope = (this.endY - this.startY) / (this.endX - this.startX);
     if (this.startX < this.endX && this.startY > this.endY) {
